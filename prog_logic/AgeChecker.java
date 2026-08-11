66321bfa6e9d6764f9ff951766a98738887b2a9b
Gabi <gbdp3300@gmail.com>
2026-08-11 18:33:33 -0300

    Uploding exercises

---
+++ prog_logic/AgeChecker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AgeChecker {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Minor");
+        }
+
+        scanner.close();
+    }
+}
